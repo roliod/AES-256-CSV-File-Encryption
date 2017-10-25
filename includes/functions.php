ecc769957eca27d8151b33bd19ffe5b8fcb9b8db
roliod <rolandoduberu@gmail.com>
2017-10-25 09:50:12 +0100

    initial commit

---
+++ includes/functions.php
@@ -0,0 +1,53 @@
+<?php
+
+$error_msg = "";
+
+//check for post data
+if ($_FILES && ! empty($_POST['enc_key'])) {
+
+    /*
+     * The variables we need
+     *
+     */
+
+    $iv = random_bytes(16);
+    $cipher = "AES-256-CBC";
+    $key = $_POST['enc_key']; //heres the encryption key
+    $filename = $_FILES["batch"]["name"];
+    $content = file_get_contents($_FILES["batch"]["tmp_name"]);
+
+    return encrypt_file($content, $iv, $key, $cipher, $filename);
+}
+
+function encrypt_file($value, $iv, $key, $cipher, $filename, $serialize = true)
+{
+    //were the magic happens
+    $value = \openssl_encrypt(
+        $serialize ? serialize($value) : $value,
+        $cipher, $key, 0, $iv
+    );
+
+    if ($value === false) {
+       return false;
+    }
+
+    //just trying to make sure we can decrypt if we need to
+    $mac = hash_mac($iv = base64_encode($iv), $value, $key);
+
+    $json = json_encode(compact('iv', 'value', 'mac'));
+
+    if (! is_string($json)) {
+        return false;
+    }
+
+    $encrypted_content = base64_encode($json);
+
+    $timestamp = strtotime(date('d-m-y H:i:s'));
+
+    return file_put_contents('files/'.$timestamp. '_encrypted.csv', $encrypted_content);
+}
+
+function hash_mac($iv, $value, $key)
+{
+    return hash_hmac('sha256', $iv.$value, $key);
+}
---
+++ index.php
@@ -0,0 +1,109 @@
+<?php
+    include_once('includes/functions.php');
+
+    $error_msg = "";
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <title>AES Encryption Alogarithm </title>
+
+    <!-- styles -->
+    <link href="assets/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="assets/libs/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="assets/css/main.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="assets/css/template.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="assets/css/encrypt.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="assets/libs/Ionicons/css/ionicons.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="assets/css/fileinput.min.css" rel="stylesheet" type="text/css" media="all" />
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+  <header class="main-header">
+    <nav class="navbar navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+              <a href="/" class="navbar-brand">
+                  AES - 256 <b>ENCRYPTION</b>
+              </a>
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                  <i class="fa fa-bars"></i>
+              </button>
+          </div>
+      </div>
+    </nav>
+  </header>
+  <div class="content-wrapper">
+    <div class="container">
+        <section class="content-header">
+          <h1>Encrypt CSV File </h1>
+        </section>
+        <section class="content">
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="bs-callout bs-callout-warning">
+                <h4>Please Note</h4>
+                Encrypted files are saved in the 'files' Folder
+              </div>
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <div class="clearfix">
+                            <h3 class="box-title">Upload CSV File</h3>
+                        </div>
+                    </div>
+                    <div class="box-body">
+                        <form method="POST" action="" accept-charset="UTF-8" enctype="multipart/form-data">
+                            <div class="form-group ">
+                                <label for="batch" class="control-label required">CSV File</label>
+                                <input class="file" id="upload-form" data-show-preview="true" data-max-file-size="0" name="batch" type="file" required>
+                                <p class="help-block"> <?php if (isset($done)) { echo $done; }?></p>
+                            </div>
+                            <div class="form-group ">
+                                <label for="enc_key" class="control-label required">Encyrption Key</label>
+                                <input class="form-control" name="enc_key" type="text" required>
+                            </div>
+                            <input class="btn btn-primary btn-lg btn-flat" type="submit" value="Encrypt batch">
+                        </form>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- footer-->
+      <footer class="main-footer">
+        <div class="text-center hidden-xs">
+          <strong>Copyright &copy; <?= date('Y'); ?> <a href="http://www.softalliance.com" target="_blank">Soft Alliance</a>.</strong>
+          All rights reserved.
+        </div>
+      </footer>
+
+      <!--scripts-->
+      <script src="assets/js/jquery-2.2.3.min.js"></script>
+      <script src="assets/js/piexif.min.js"></script>
+      <script src="assets/js/fileinput.min.js"></script>
+      <script src="assets/libs/bootstrap/js/bootstrap.min.js"></script>
+      <script>
+        $("#upload-form").fileinput({
+          'showUpload': false,
+          'showPreview': true,
+          'showDrag': true,
+          allowedFileExtensions: ["csv"],
+        });
+      </script>
+   </div>
+</body>
+</html>
